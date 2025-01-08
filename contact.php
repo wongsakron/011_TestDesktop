12d867f419ab26ce5c451bc01db810c3d0e48388
Wongsakron <skizz3848@gmail.com>
2025-01-08 13:57:31 +0700

    แก้ไขไฟลืครั้งที่ #1

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@
                 <img src="images/map.gif">
             </td>
             <td width="190" bgcolor="#FFFF66"><ul>
-				<li><a href="home.php">หน้าแรก</li>
+				<li><a href="home.php">หน้าแรก</li>sdgsdg
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a href="contact.php">ติดต่อ</li>
